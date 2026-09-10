4e2a9d2c5bbb9265548f1b66a1cdf67e2462fa1a
Mayank-Nagar2007 <nagarmayank1411@gmail.com>
2026-09-10 18:25:35 +0530

    Update POSTGRESQL_PROJECT_1.sql

--- POSTGRESQL_PROJECT_1.sql
+++ POSTGRESQL_PROJECT_1.sql
@@ -35,37 +35,36 @@ SELECT * FROM customers;
 
 
 
--- BASIC QUERIES
--- Q1
+
 SELECT book_id,genre 
 FROM books
   where genre = 'Fiction';
 
 
-  --  Q2
+  
 SELECT book_id,genre ,published_year
  FROM books 
  where published_year >'1950' ;
 
 
--- Q3 
+
 SELECT customer_id, name,country 
 FROM customers 
 WHERE country = 'Canada';
 
---Q4 
+
 -- SELECT order_id, customer_id,book_id,
 --    sum(quantity) AS sum_of 
 --    FROM orders
 --    where order_date BETWEEN '2023-11-01' AND '2023-11-30'
 --    GROUP BY order_id,customer_id,book_id ;
 
--- Q5 
+
 SELECT 
    SUM(stock) AS total_sum
    FROM books ;
 
--- Q6 
+ 
 SELECT * FROM books ORDER BY price DESC LIMIT 1;
 
 -- with higest_price AS(
@@ -77,31 +76,28 @@ SELECT * FROM books ORDER BY price DESC LIMIT 1;
 --    WHERE rnk = 1;
 
 
--- Q7
 SELECT * FROM orders 
 WHERE quantity >1;
 
 
--- Q8
+
 SELECT * FROM orders 
 WHERE total_amount > 20;
 
--- Q9
+
 SELECT genre FROM books 
     GROUP BY genre ;
 
---Q10
+
 SELECT * FROM books ORDER BY stock ASC LIMIT 1;
 
--- Q11
 SELECT 
 SUM(total_amount) AS Revenue 
  FROM orders ;
 
 
 
--- ADVANCE QUESTION
--- Q1 
+
 SELECT DISTINCT b.genre,sum(o.quantity)  AS total_book_sold
   FROM books b
   INNER JOIN 
@@ -110,7 +106,7 @@ SELECT DISTINCT b.genre,sum(o.quantity)  AS total_book_sold
    GROUP BY b.genre ;
 
 
--- Q2
+
 WITH average_price_genre AS(
 SELECT DISTINCT b.genre,AVG(b.price) as avg_price
 FROM books b
@@ -120,14 +116,6 @@ GROUP BY genre
 WHERE genre = 'Fantasy';
 
 
--- Q3
--- SELECT c.name,c.customer_id,o.quantity 
--- FROM customers c 
--- JOIN 
--- orders o 
--- ON c.customer_id = o.customer_id 
--- WHERE o.quantity >=2;
-
 
 SELECT o.customer_id,c.name,COUNT(o.order_id) AS ORDER_COUNT 
 FROM orders o   
@@ -135,14 +123,13 @@ JOIN  customers c ON o.customer_id = c.customer_id
 GROUP BY o.customer_id,c.name 
 HAVING COUNT(order_id) >=2;
 
--- Q4
+
 SELECT o.book_id,b.title.count(o.order_id) AS ORDER_COUNT
 FROM orders o 
 JOIN books b ON o.book_id = b.book_id 
 GROUP BY o.book_id ,b.title 
 ORDER BY ORDER_COUNT DESC LIMIT 1;
 
--- Q5
 SELECT  book_id ,genre,price  
   FROM books 
   GROUP BY genre,book_id ,price
@@ -150,14 +137,14 @@ SELECT  book_id ,genre,price
   ORDER BY price DESC LIMIT 3;
 
 
-  -- Q6
+  
 SELECT DISTINCT b.author ,sum(o.quantity) AS each_quantity
 FROM books b 
 JOIN orders o ON o.book_id = b.book_id 
 GROUP BY b.author;
 
 
--- Q7
+
 SELECT DISTINCT c.city ,sum(o.total_amount) AS total_amo
  FROM orders o    
  JOIN customers c ON o.customer_id = c.customer_id 
@@ -166,7 +153,7 @@ SELECT DISTINCT c.city ,sum(o.total_amount) AS total_amo
 
 
 
---  Q8
+
 SELECT c.customer_id,c.name , SUM(o.total_amount) AS Total_spent 
 FROM orders o  
 JOIN  customers c ON o.customer_id = c.customer_id 
@@ -174,7 +161,7 @@ GROUP BY c.customer_id,c.name
 ORDER BY Total_spent DESC LIMIT 1;
 
 
--- Q9
+
 SELECT b.book_id,b.title , b.stock , COALESCE(SUM(o.quantity),0) AS order_quntity ,
   b.stock -COALESCE(SUM(o.quantity),0) AS remainig_quantity 
 FROM books b 
